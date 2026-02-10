633dd295968362bd9fac90dd25234347fcdb6546
GamerLucas2 <lucasonckent@hotmail.com>
2026-02-10 15:26:44 -0300

    Main Scene

---
+++ FlappyBird-2026/Assets/Scripts/PlayerBehaiviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerBehaiviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Never gonna give you up. Never gonna let you down. Never gonna run around...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
